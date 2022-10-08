1edff290ff1cb4f6f41a30b051a2c9150e60ef72
Ster-bent <104021908+Ster-bent@users.noreply.github.com>
2022-10-08 17:43:11 -0500

    Update scor_logic.py
    Added some changes will update later

--- scor_logic.py
+++ scor_logic.py
@@ -1,3 +1,19 @@
 value = int(input())
 value2 = int(input())
 operator = str(input())
+
+def addition(value,value2):
+  print(value+value2)
+  return
+
+def subtraction(value,value2):
+  print(value-value2)
+  return
+
+def multiplication(value,value2):
+  print(value*value2)
+  return
+
+def division(value,value2):
+  print(value/value2)
+  return
