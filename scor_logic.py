665c3688d9666c719e626401eec2e604267af22d
Ster-bent <104021908+Ster-bent@users.noreply.github.com>
2022-10-09 19:05:56 -0500

    Updated scor_logic.py
    added some much needed logic for calculations, adding a sigfigs operation

--- scor_logic.py
+++ scor_logic.py
@@ -17,3 +17,24 @@ def multiplication(value,value2):
 def division(value,value2):
   print(value/value2)
   return
+
+def exponent(value,value2):
+  print(value**value2)
+  return
+
+#actual logic
+
+if operator == '+':
+  addition(value,value2)
+elif operator == '-':
+  subtraction(value,value2)
+elif operator == '*':
+  multiplication(value,value2)
+elif operator == '/':
+  division(value,value2)
+elif operator == '^':
+  exponent(value,value2)
+else:
+  print(invalid operation)
+  
+print('operation complete')
